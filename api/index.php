2873a3e261a0d177756dc34c26e44eb554a277f3
rzrgames <110288747+rzrgames@users.noreply.github.com>
2023-07-08 16:35:41 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>The Window Object</h1>
+<h2>The alert() Method</h2>
+
+<p>Click the button to display an alert box.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert("Hello! I am an alert box!");
+}
+</script>
+
+</body>
+
 <?php
 
 //require_once __DIR__ . '/../vendor/autoload.php';
